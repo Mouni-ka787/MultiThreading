a07a4ae0deba472d781c217f2d3fcacefea338c9
Mouni-ka787 <mounika2004117@gmail.com>
2025-08-05 15:37:50 -1200

    singlethread

--- SingelThreading/Server.java
+++ SingelThreading/Server.java
@@ -1,5 +1,49 @@
 package MultiThreading.SingelThreading;
 
-public  {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Server {
     
+    public void run() throws IOException, UnknownHostException{
+        int port = 8010;
+        ServerSocket socket = new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        while(true){
+            try{
+            System.out.println("Server is listening on port: "+port);
+            Socket acceptedConnection = socket.accept();
+            System.out.println("Connected accepted from client "+acceptedConnection.getRemoteSocketAddress());
+            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            toClient.println("Hello World from the server");
+            toClient.close();
+            fromClient.close();
+            acceptedConnection.close();
+        
+            }
+          catch (IOException ex) {
+            ex.printStackTrace();
+            // TODO: handle exception
+        }
+        
+        }
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        try{
+            server.run();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
+
+
